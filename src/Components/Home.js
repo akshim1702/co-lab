4ddbf19b1a576619d6e20420ef6305e9cb0b9aa8
KHYATI SHARMA <khyatisharma2001514@gmail.com>
2021-09-17 16:57:21 +0530

    slick slider css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,17 +3,17 @@ import Slider from "react-slick";
 import './Home.css';
 const Home = () => {
     const settings = {
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 200,
-            dots: true,
-            prevArrow: <button type="button" className="slick-prev">Previous</button>,
-            nextArrow: <button type="button" className="slick-prev">Previous</button>,
-            centerMode: true,
-            centerPadding: '0,'
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 200,
+        dots: false,
+        prevArrow: <button type="button" className="slick-prev">Previous</button>,
+        nextArrow: <button type="button" className="slick-prev">Previous</button>,
+        centerMode: true,
+        centerPadding: '0,'
     };
     return (
         <>
@@ -32,31 +32,37 @@ const Home = () => {
 
             <div className="secondSection">
                 <div className="for_slick_slider">
-                    
+
                     <Slider {...settings}>
                         <div className="itemsl">
-                            <h3>hello</h3>
-                            <div class="overlay"></div>
+                            <div className="slider_head">
+                                <h3>hello</h3>
+                            </div>
                         </div>
-                        <div>
-                            <h3>hey</h3>
-                            <div class="overlay"></div>
+                        <div className="itemsl">
+                            <div className="slider_head">
+                                <h3>hey</h3>
+                            </div>
                         </div>
-                        <div>
-                            <h3>hola</h3>
-                            <div class="overlay"></div>
+                        <div className="itemsl">
+                            <div className="slider_head">
+                                <h3>hola</h3>
+                            </div>
                         </div>
-                        <div>
-                            <h3>si</h3>
-                            <div class="overlay"></div>
+                        <div className="itemsl">
+                            <div className="slider_head">
+                                <h3>si</h3>
+                            </div>
                         </div>
-                        <div>
-                            <h3>namste</h3>
-                            <div class="overlay"></div>
+                        <div className="itemsl">
+                            <div className="slider_head">
+                                <h3>namste</h3>
+                            </div>
                         </div>
-                        <div>
-                            <h3>bejnfsnndsndk|</h3>
-                            <div class="overlay"></div>
+                        <div className="itemsl">
+                            <div className="slider_head">
+                                <h3>bsdk</h3>
+                            </div>
                         </div>
                     </Slider>
                 </div>
